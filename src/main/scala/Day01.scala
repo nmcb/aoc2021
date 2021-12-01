38ee66e131611375b7809e955aadb25eb0883ab4
nmcb <marco@splatter.nl>
2021-12-01 07:10:22 +0100

    added solutions day 01

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -4,13 +4,29 @@ object Day01 extends App {
 
   val start1 = System.currentTimeMillis
 
-  val answer1: Int =
-      Source
-        .fromFile("src/resources/input01.txt")
-        .getLines
-        .map(_.trim.toInt)
-        .sum
+  val depths: List[Int] =
+    Source
+      .fromFile("src/resources/input01.txt")
+      .getLines
+      .map(_.trim.toInt)
+      .toList
+  
+  def incs(l: List[Int], n: Int = 0, p: Int = 0): Int =
+    if (l.isEmpty)
+      n - 1
+    else if (l.head > p)
+      incs(l.tail, n + 1, l.head)
+    else
+      incs(l.tail, n, l.head)
 
-  println(s"Answer part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
+  println(s"Answer part 1: ${incs(depths)} [${System.currentTimeMillis - start1}ms]")
+
+  val answer2: List[Int] =
+    depths
+      .sliding(3)
+      .map(_.sum)
+      .toList
+
+  println(s"Answer part 2: ${incs(answer2)} [${System.currentTimeMillis - start1}ms]")
 
 }
