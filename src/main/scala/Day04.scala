0f4099b4e7852211ac8ae375c0401d34fe3a9660
nmcb <marco@splatter.nl>
2021-12-04 11:48:01 +0100

    flexing

--- src/main/scala/Day04.scala
+++ src/main/scala/Day04.scala
@@ -3,69 +3,73 @@ import scala.io._
 object Day04 extends App:
   val start = System.currentTimeMillis
 
-  val Zero = '0'
-  val One  = '1'
-
-  val draws: List[Int] =
+  val draws =
     Source
       .fromFile("src/resources/input04.txt")
       .getLines
-      .toList(0).trim.split(",").map(_.toInt)
+      .toList(0).trim.split(",").map(_.trim.toInt)
       .toList
 
-  case class Board(rows: List[List[Int]] = List.empty, draws: List[Int] = List.empty):
-    def hasRowWith(draws: List[Int]): Boolean =
-      rows.filter(_.forall(draws.contains _)).nonEmpty
-    def hasColumnWith(draws: List[Int]): Boolean =
-      rows.transpose.filter(_.forall(draws.contains _)).nonEmpty
-    def draw(num: Int): Board =
-      if (!hasWon) copy(draws = draws :+ num) else this
-    def hasWon: Boolean =
-      hasRowWith(draws) || hasColumnWith(draws)
-    def unmarked: List[Int] =
-      rows.flatten.filterNot(draws.contains)
-    def lastDraw: Int =
-      draws.last
-    
-      
-  val boards: List[Board] =
+  val Line =
+    """(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)""".r 
+
+  def boards =
     Source
       .fromFile("src/resources/input04.txt")
       .getLines
       .drop(2)
       .toList
-      .map(l => if (l.trim.isEmpty) List.empty else l.trim.split("\\s+").map(_.toInt).toList)
-      .foldLeft(List(Board()))((acc,row) =>
-        if (row.isEmpty)
-          acc :+ Board()
-        else
-          acc.init :+ Board(acc.last.rows :+ row))
-      .init
+      .map(_.trim)
+      .foldLeft((List.empty[Board[Int]], Board.empty[Int])) {
+        case ((bs,b), "") =>
+          ((b :: bs), Board.empty)
+        case ((bs,b), Line(n0,n1,n2,n3,n4)) =>
+          (bs, b.addRow(
+            List(n0.toInt,n1.toInt,n2.toInt,n3.toInt,n4.toInt)))
+        case ((_,_),_) =>
+          sys.error("boom!")
+      }._1
+  case class Board[A](rows: List[List[A]], draws: List[A] = List.empty):
+    def addRow(row: List[A]): Board[A] =
+      copy(rows = rows :+ row)
+    def draw(num: A): Board[A] =
+      if (!hasBingo) copy(draws = draws :+ num) else this
+    def hasBingo: Boolean =
+      hasRowWith(draws) || hasColumnWith(draws)
+    def unmarked: List[A] =
+      rows.flatten.filterNot(draws.contains)
+    def lastDraw: A =
+      draws.last
+    private def hasRowWith(draws: List[A]): Boolean =
+      rows.filter(_.forall(draws.contains _)).nonEmpty
+    private def hasColumnWith(draws: List[A]): Boolean =
+      rows.transpose.filter(_.forall(draws.contains _)).nonEmpty
 
+  object Board:
+    def empty[A]: Board[A] =
+      Board[A](List.empty)
 
+  def playWhoWinsFirst[A](draws: List[A], boards: List[Board[A]] = boards): Board[A] =
+    val round = boards.map(_.draw(draws.head))
+    round
+      .filter(_.hasBingo)
+      .headOption
+      .getOrElse(playWhoWinsFirst(draws.tail, round))
 
-  def first(draws: List[Int], game: List[Board] = boards): Board =
-    val next = game.map(_.draw(draws.head))
-    val won  = next.filter(_.hasWon).headOption
-    if (won.isDefined)
-      won.get
-    else
-      first(draws.tail, next)
-
-  val answer1  = first(draws)
+  val answer1 =
+    playWhoWinsFirst(draws)
 
   println(s"Answer 1 = ${answer1.unmarked.sum * answer1.lastDraw} [${System.currentTimeMillis - start}ms]")
 
-  def last(draws: List[Int], game: List[Board] = boards): Board =
-    val next = game.map(_.draw(draws.head))
-    val todo = next.filterNot(_.hasWon)
-    println("--todo--")
-    println(todo.mkString("\n"))
+  def playWhoWinsLast[A](draws: List[A], game: List[Board[A]] = boards): Board[A] =
+    val round = game.map(_.draw(draws.head))
+    val todo = round.filterNot(_.hasBingo)
     if (todo.size > 0)
-      last(draws.tail, todo)
+      playWhoWinsLast(draws.tail, todo)
     else 
-      next.filter(_.lastDraw == draws.head).head
+      round.filter(_.lastDraw == draws.head).head
 
-  def answer2 = last(draws)
+  def answer2 =
+    playWhoWinsLast(draws)
 
   println(s"Answer 2 = ${answer2.unmarked.sum * answer2.lastDraw} [${System.currentTimeMillis - start}ms]")
