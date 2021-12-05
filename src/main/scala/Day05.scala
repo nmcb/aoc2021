e83b02c03d3463fd32d7355f4f6efe98970bc055
nmcb <marco@splatter.nl>
2021-12-05 19:11:04 +0100

    cause im beautiful .. in each and every way ..

--- src/main/scala/Day05.scala
+++ src/main/scala/Day05.scala
@@ -1,4 +1,5 @@
 import scala.io._
+import scala.collection.immutable.HashSet
 
 object Day05 extends App:
   val start = System.currentTimeMillis
@@ -13,29 +14,46 @@ object Day05 extends App:
       .toList
       .map(_.trim)
       .map {
+        case LineLit(x0, y0, x1, y1) if y0 == y1 =>
+          HLine(x0.toInt, y0.toInt, x1.toInt, y1.toInt)
+        case LineLit(x0, y0, x1, y1) if x0 == x1 =>
+          VLine(x0.toInt, y0.toInt, x1.toInt, y1.toInt)
         case LineLit(x0, y0, x1, y1) =>
-          Line(x0.toInt, y0.toInt, x1.toInt, y1.toInt)
+          DLine(x0.toInt, y0.toInt, x1.toInt, y1.toInt)
       }
 
-  case class Line(x0: Int, y0: Int, x1: Int, y1: Int):
-    val maxX: Int = math.max(x0, x1)
-    val maxY: Int = math.max(y0, y1)
-    val minX: Int = math.min(x0, x1)
-    val minY: Int = math.min(y0, y1)
-    val isHorizontal: Boolean = y0 == y1
-    val isVertical: Boolean   = x0 == x1
+  sealed trait Line:
+    val points: Set[(Int,Int)]
+
+  case class HLine(x0: Int, y0: Int, x1: Int, y1: Int)
+    extends Line:
     val points: Set[(Int,Int)] =
-      if (isHorizontal)
-        if (x1 >= x0) (x0 to x1).map((_, y0)).toSet
-        else (x1 to x0).map((_, y0)).toSet
-      else if (isVertical)
-        if (y1 >= y0) (y0 to y1).map((x0, _)).toSet
-        else (y1 to y0).map((x0, _)).toSet
-      else (0 to (maxX - minX)).map( n =>
-        if (x1 > x0)
-          if (y1 > y0) (x0 + n, y0 + n) else (x0 + n, y0 - n)
+      if (x0 < x1)
+        (x0 to x1).map((_, y0)).toSet
+      else
+        (x1 to x0).map((_, y0)).toSet
+  case class VLine(x0: Int, y0: Int, x1: Int, y1: Int)
+    extends Line:
+    val points: Set[(Int,Int)] =
+      if (y0 < y1)
+        (y0 to y1).map((x0, _)).toSet
+      else
+        (y1 to y0).map((x0, _)).toSet
+  case class DLine(x0: Int, y0: Int, x1: Int, y1: Int)
+    extends Line:
+
+    val points: Set[(Int,Int)] =
+      (0 to (math.max(x0, x1) - math.min(x0, x1))).map( n =>
+        if (x0 < x1)
+          if (y0 < y1)
+            (x0 + n, y0 + n)
+          else
+            (x0 + n, y0 - n)
         else
-          if (y1 > y0) (x0 - n, y0 + n) else (x0 - n, y0 - n)
+          if (y0 < y1)
+            (x0 - n, y0 + n)
+          else
+            (x0 - n, y0 - n)
       ).toSet
 
   case class Floor(locations: Map[(Int,Int), Int]):
