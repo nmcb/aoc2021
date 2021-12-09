664ee57e317dede8b9a597e3f2c072067fd2d440
nmcb <marco@splatter.nl>
2021-12-09 12:13:08 +0100

    added solutions day 9

---
+++ src/main/scala/Day09.scala
@@ -0,0 +1,68 @@
+import scala.io._
+
+object Day09 extends App:
+  val start = System.currentTimeMillis
+
+  val floor =
+    Floor(Source
+      .fromFile("src/resources/input09.txt")
+      .getLines
+      .map(line => line.map(code => Integer.parseInt(code.toString)).toList)
+      .toList)
+
+  case class Floor(loc: List[List[Int]]):
+    val maxX = loc.head.size
+    val maxY = loc.size
+
+    def sample(x: Int, y: Int): Option[Int] =
+      if (y < 0 || y >= maxY) None
+      else if (x < 0 || x >= maxX) None
+      else Some(loc(y)(x))
+
+    def height(x: Int, y: Int): Int =
+      sample(x,y).getOrElse(sys.error("boom!"))
+
+    def NeighbourHeights(x: Int, y: Int): List[((Int,Int), Int)] =
+      List((1,0),(-1,0),(0,1),(0,-1))
+        .map((nx,ny) => ((nx,ny), sample(x + nx, y + ny)))
+        .filterNot(_._2 == None)
+        .map{ case ((nx,ny),h) => ((x + nx,y + ny),h.get) }
+
+    def upstream(x: Int, y: Int): List[(Int,Int)] =
+      NeighbourHeights(x,y)
+        .filter{ case ((nx,ny),nh) => (nh < 9) && (nh > height(x,y)) }
+        .map(_._1)
+        
+  val heights =
+    var result: List[((Int,Int),Int)] = List.empty // shoot me!
+    for (x <- (0 until floor.maxX) ; y <- (0 until floor.maxY)) {
+      val height = floor.height(x, y)
+      if (floor.NeighbourHeights(x, y).forall { case ((nx,ny),nh) => height < nh })
+        result = ((x,y), height) :: result
+      else
+        ()
+    }
+    result
+
+  val answer1 = heights.map((_,h) => h + 1).sum
+  println(s"Answer 1 = ${answer1} [${System.currentTimeMillis - start}ms]")
+  assert(answer1 == 502)
+
+  val answer2 = 
+    def loop(todo: List[(Int,Int)], acc: List[(Int,Int)] = List.empty): Int =
+      if (todo.isEmpty)
+        acc.size
+      else
+        val (x, y)   = todo.head
+        val upstream = floor.upstream(x, y)
+        loop(upstream ++ todo.tail, ((x,y) :: acc).distinct)
+    
+    heights
+      .map((point,h) => loop(List((point._1,point._2))))
+      .sorted
+      .reverse
+      .take(3)
+      .product
+
+  println(s"Answer 2 = ${answer2} [${System.currentTimeMillis - start}ms]")
+  assert(answer2 == 1330560)
