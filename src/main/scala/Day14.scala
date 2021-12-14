ebb62a330e2fc30dd2cd9b3a50b513a11786f201
nmcb <marco@splatter.nl>
2021-12-14 23:11:02 +0100

    explicit typing chore

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -3,63 +3,55 @@ import scala.io._
 object Day14 extends App:
   val start = System.currentTimeMillis
 
-  val template: String =
+  val lines: List[String] =
     Source
       .fromFile("src/resources/input14.txt")
       .getLines
       .toList
-      .head
-      .trim
 
-  type Pairs  = Map[(Char, Char), Long]
-  type Rules  = Map[(Char, Char), Char]
-  type Counts = Map[Char, Long]
-  object Counts:
-    def empty = Map.empty[Char, Long]
+  val template: String =
+    lines.head.trim
 
-  case class Poly(upairs: Pairs, ucounts: Counts, urules: Rules):
-    def max = ucounts.values.max
-    def min = ucounts.values.min
+  val rules: Map[(Char, Char), Char] =
+    lines.drop(2)
+      .map( line =>
+        val Array(pair,insert) = line.trim.split("->").map(_.trim)
+        (pair(0), pair(1)) -> insert.head
+      )
+      .toMap
 
-    def next: Poly =
-      val npairs: Pairs =
-        upairs
-          .toList
-          .flatMap({
-            case (pair@(a,b),count) =>
-              val c = urules(pair)
-              List((a, c) -> count, (c, b) -> count)
-          }).groupMapReduce(_._1)(_._2)(_+_)
+  def next(pairs: Map[(Char, Char), Long], counts: Map[Char, Long]): (Map[(Char, Char), Long], Map[Char, Long]) =
+    val npairs: Map[(Char, Char), Long] =
+      pairs.toList.flatMap({ case (pair@(a, b), count) =>
+        val c = rules(pair)
+        List((a, c) -> count, (c, b) -> count)
+      }).groupMapReduce(_._1)(_._2)(_+_)
 
-      val ncounts: Counts =
-        upairs
-          .foldLeft(ucounts)({
-            case (acc0, (pair@(a, b), count)) =>
-              val c = urules(pair)
-              acc0 + (c -> (acc0.getOrElse(c, 0L) + count))
-          }).groupMapReduce(_._1)(_._2)(_+_) 
+    val ncounts: Map[Char, Long] =
+      pairs.foldLeft(counts)({ case (acc, (pair@(a, b), count)) =>
+        val char = rules(pair)
+        acc.updated(char, acc.getOrElse(char, 0L) + count)
+      }).groupMapReduce(_._1)(_._2)(_+_) 
 
-      Poly(npairs, ncounts, urules)
+    (npairs, ncounts)
 
-  val lpairs: Pairs =
+  def max(counts: Map[Char, Long]) =
+    counts.values.max
+    
+  def min(counts: Map[Char, Long]) =
+    counts.values.min
+
+  val initPairs: Map[(Char, Char), Long] =
     template
       .zip(template.tail)
       .groupMapReduce(identity)(_ => 1L)(_+_)
-      
-  val lrules: Rules =
-    Source
-      .fromFile("src/resources/input14.txt")
-      .getLines
-      .toList
-      .drop(2)
-      .map(_.trim.split("->").map(_.trim))
-      .map(l => (l(0)(0), l(0)(1)) -> l(1).head)
-      .toMap
 
-  val lcounts: Counts =
+  val initCounts: Map[Char, Long] =
     template.groupMapReduce(identity)(_ => 1L)(_ + _)
+        
+  val (pairs, counts) =
+    (1 to 40).foldLeft((initPairs, initCounts))({
+      case ((pairs,counts), index) => next(pairs, counts)
+    })
 
-  val poly = Poly(lpairs, lcounts, lrules)
-
-  val result = (1 to 40).foldLeft(poly)((p,i) => p.next)
-  println(result.max - result.min)
+  println(max(counts) - min(counts))
