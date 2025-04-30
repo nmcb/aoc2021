7769071166bebc54f2eaf6914a782fa94dfd03e0
nmcb <nmcb@xs4all.nl>
2025-04-30 09:29:42 +0200

    cleanup day 02

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -1,44 +1,59 @@
 import scala.io._
 
 object Day02 extends App:
-  val start = System.currentTimeMillis
 
-  sealed abstract class Inst(delta: Long)
-  case class Up(delta: Long)      extends Inst(delta)
-  case class Down(delta: Long)    extends Inst(delta)
-  case class Forward(delta: Long) extends Inst(delta)
+  val day: String =
+    getClass.getSimpleName.filter(_.isDigit)
+
+  enum Inst:
+    case Up(delta: Long)
+    case Down(delta: Long)
+    case Forward(delta: Long)
+
+  import Inst.*
 
   object Inst:
     def fromLine(l: String): Inst =
-      l.split(" ").toList match
-        case command :: d :: Nil if command.startsWith("up")      => Up(d.toLong)
-        case command :: d :: Nil if command.startsWith("down")    => Down(d.toLong)
-        case command :: d :: Nil if command.startsWith("forward") => Forward(d.toLong)
-        case _ => sys.error(s"error parsing line: $l")
+      l match
+        case s"up $d"      => Up(d.toLong)
+        case s"down $d"    => Down(d.toLong)
+        case s"forward $d" => Forward(d.toLong)
 
   case class Sub(heading: Long = 0, depth: Long = 0, aim: Long = 0):
-    def move(is: List[Inst]): Sub =
-      is.foldLeft(this)((cur,is) => is match
-        case Up(d) =>
-          cur.copy( aim = cur.aim - d)
-        case Down(d) =>
-          cur.copy( aim = cur.aim + d)
-        case Forward(d) =>
-          cur.copy( heading = cur.heading + d, depth = cur.depth + (cur.aim * d)))
 
-  object Sub {
-    def init: Sub = Sub()
-  }
+    def dive(n: Long): Sub = copy(depth = depth + n)
+    def move(n: Long): Sub = copy(heading = heading + n)
+    def tilt(n: Long): Sub = copy(aim = aim + n)
+
+    def run(is: List[Inst], part2: Boolean = false): Sub =
+      is.foldLeft(this): (cur,is) =>
+        is match
+          case Up(d) =>
+            if part2 then cur.tilt(-d) else cur.dive(-d)
+          case Down(d) =>
+            if part2 then cur.tilt(d) else cur.dive(d)
+          case Forward(d) =>
+            if part2 then cur.move(d).dive(cur.aim * d) else cur.move(d)
+
+    def solution: Long =
+      heading * depth
+
+  object Sub:
+    val init: Sub = Sub()
 
   val instructions: List[Inst] =
     Source
-      .fromFile("src/main/resources/input02.txt")
+      .fromResource(s"input$day.txt")
       .getLines
       .map(Inst.fromLine)
       .toList
 
-  val answer: Long =
-    val sub = Sub.init.move(instructions)
-    sub.heading * sub.depth
-  
-  println(s"Answer: ${answer} [${System.currentTimeMillis - start}ms]")
+
+  val start1  = System.currentTimeMillis
+  val answer1 = Sub.init.run(instructions).solution
+  println(s"Answer part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
+
+  val start2  = System.currentTimeMillis
+  val answer2 = Sub.init.run(instructions, part2 = true).solution
+  println(s"Answer part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
+
