72da1699a8759d0b460ef0f6d2d84a56b4e3e272
nmcb <nmcb@xs4all.nl>
2025-05-12 00:13:16 +0200

    cleanup day 21

--- src/main/scala/Day21.scala
+++ src/main/scala/Day21.scala
@@ -1,5 +1,3 @@
-import scala.collection.*
-
 object Day21 extends App:
 
   val day: String =
@@ -16,7 +14,7 @@ object Day21 extends App:
       else
         copy(cur = cur + 1, rolled = rolled + 1)
 
-  case class Player(name: String, start: Int, rolled: Vector[Int] = Vector.empty, score: Int = 0)
+  case class Player(name: String, start: Int, rolled: Seq[Int] = Seq.empty, score: Int = 0)
 
   case class Game1(player1: Player, player2: Player, pawn1: Int, pawn2: Int, dice: Dice, goal: Int)
 
@@ -45,51 +43,53 @@ object Day21 extends App:
       )
 
 
-  val play1 = Player(name = "#1", start = 7)
-  val play2 = Player(name = "#2", start = 9)
-  val game1: Game1 = Game1(play1, play2, play1.start, play2.start, Dice(1, 100), 1000)
+  val player1 = Player(name = "#1", start = 7)
+  val player2 = Player(name = "#2", start = 9)
+  val game1   = Game1(player1, player2, player1.start, player2.start, Dice(1, 100), 1000)
+
 
   val start1  = System.currentTimeMillis
   val answer1 = solve1(game1)
-  println(s"Day $day answer 1: $answer1 [${System.currentTimeMillis - start1}ms]")
+  println(s"answer 1: $answer1 [${System.currentTimeMillis - start1}ms]")
+
 
   case class Pawn(pos: Int, score: Int = 0):
+
     def move(steps: Int): Pawn =
-      val npos = ((pos - 1 + steps) % 10) + 1 
+      val npos = ((pos - 1 + steps) % 10) + 1
       Pawn(npos, score + npos)
 
-  val roll3x3x3: Map[Int,Int] =
-
-    val rolls =
+  val roll3x3x3 =
+    val throws =
       for
         t1 <- 1 to 3
         t2 <- 1 to 3
         t3 <- 1 to 3
       yield t1 + t2 + t3
 
-    rolls.groupMapReduce(identity)(_ => 1)(_ + _)
+    throws.groupMapReduce(identity)(_ => 1)(_ + _)
 
   def solve2(pawn1: Pawn, pawn2: Pawn): Long =
 
-    def getOrTurn(pawn1: Pawn, pawn2: Pawn): (Long, Long) =
-      if pawn1.score >= 21 then (1, 0) else go(pawn2, pawn1).swap
-
     def go(pawn1: Pawn, pawn2: Pawn): (Long, Long) =
+
+      def getOrTurn(pawn1: Pawn, pawn2: Pawn): (Long, Long) =
+        if pawn1.score >= 21 then (1, 0) else go(pawn2, pawn1).swap
 
       val turns =
         for
           (roll, count) <- roll3x3x3
-        yield
-          val npawn1   = pawn1.move(roll)
-          val (u1, u2) = getOrTurn(npawn1, pawn2)
-          (count * u1, count * u2)
+          npawn1         = pawn1.move(roll)
+          (u1, u2)       = getOrTurn(npawn1, pawn2)
+        yield (count * u1, count * u2)
 
       turns.reduce:
         case ((u1a, u2a), (u1b, u2b)) => (u1a + u1b, u2a + u2b)
 
-    val (winner, loser) = go(pawn1, pawn2)
-    winner max loser
+    val (score1, score2) = go(pawn1, pawn2)
+    score1 max score2
 
-  val start2  = System.currentTimeMillis
+
+  val start2 = System.currentTimeMillis
   val answer2 = solve2(Pawn(pos = 7), Pawn(pos = 9))
-  println(s"Day $day answer 2: $answer2 [${System.currentTimeMillis - start2}ms]")
+  println(s"answer 2: $answer2 [${System.currentTimeMillis - start2}ms]")
