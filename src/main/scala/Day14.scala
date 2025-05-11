90781785615a595fcf03121f01681e6c563098ec
nmcb <nmcb@xs4all.nl>
2025-05-11 15:31:51 +0200

    fixed standard out

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -67,8 +67,8 @@ object Day14 extends App:
 
   val start1  = System.currentTimeMillis
   val answer1 = solve(polymer, 10)
-  println(s"Answer 1 = $answer1 [${System.currentTimeMillis - start1}ms]")
+  println(s"Day $day answer 1 = $answer1 [${System.currentTimeMillis - start1}ms]")
 
   val start2  = System.currentTimeMillis
   val answer2 = solve(polymer, 40)
-  println(s"Answer 2 = $answer2 [${System.currentTimeMillis - start2}ms]")
+  println(s"Day $day answer 2 = $answer2 [${System.currentTimeMillis - start2}ms]")
