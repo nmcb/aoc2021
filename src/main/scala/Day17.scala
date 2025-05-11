8d9526f20c8a425b4ceff173f4adb0513d51a2fc
nmcb <nmcb@xs4all.nl>
2025-05-11 22:07:51 +0200

    cleanup day 17

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -1,41 +1,32 @@
-import scala.io._
+import scala.annotation.tailrec
 
 object Day17 extends App:
-
   
-  val start = System.currentTimeMillis
+  val day = getClass.getSimpleName.filter(_.isDigit).mkString
   
   case class Probe(x: Int, y: Int, vx: Int, vy: Int):
     def step: Probe =
       val nx  = x + vx
       val ny  = y + vy
-      val nvx = if (vx > 0) vx - 1 else if (vx < 0) vx + 1 else 0
+      val nvx = if vx > 0 then vx - 1 else if vx < 0 then vx + 1 else 0
       val nvy = vy - 1
       Probe(nx, ny, nvx, nvy)
 
-  // val targetX =  20 to 30
-  // val targetY = -10 to -5
   val targetX =  150 to 193
   val targetY = -136 to -86
 
+  @tailrec
   def go(x: Int, y: Int, vx: Int, vy: Int, maxY: Int = 0): Option[Int] =
-    Probe(x, y ,vx, vy).step match {
+    Probe(x, y ,vx, vy).step match
       case Probe(nx, ny, nvx, nvy) if targetX.contains(nx) && targetY.contains(ny) => Some(maxY)
       case Probe(nx, ny, nvx, nvy) if nx > targetX.max => None
       case Probe(nx, ny, nvx, nvy) if ny < targetY.min => None
-      case Probe(nx, ny, nvx, nvy) => go(nx, ny, nvx, nvy, if (ny > maxY) ny else maxY)
-    }
+      case Probe(nx, ny, nvx, nvy) => go(nx, ny, nvx, nvy, if ny > maxY then ny else maxY)
 
-  val answer1 =
-    (0 to 50).flatMap(x => (0 to 200).map(y => (x,y))).map((vx,vy) =>
-      go(0, 0, vx, vy)
-    ).filter(_.isDefined).map(_.get).max
+  val start1  = System.currentTimeMillis
+  val answer1 = (0 to 50).flatMap(vx => (0 to 200).flatMap(vy => go(0, 0, vx, vy))).max
+  println(s"Day $day answer 1 = $answer1 [${System.currentTimeMillis - start1}ms]")
 
-  println(s"Answer 1 = ${answer1} [${System.currentTimeMillis - start}ms]")
-
-  val answer2 =
-    (0 to 200).flatMap(x => (-200 to 200).map(y => (x,y))).map((vx,vy) =>
-      go(0, 0, vx, vy)
-    ).filter(_.isDefined)
-
-  println(s"Answer 2 = ${answer2.size} [${System.currentTimeMillis - start}ms]")
+  val start2  = System.currentTimeMillis
+  val answer2 = (0 to 200).flatMap(vx => (-200 to 200).flatMap(vy => go(0, 0, vx, vy))).size
+  println(s"Day $day answer 2 = $answer2 [${System.currentTimeMillis - start2}ms]")
